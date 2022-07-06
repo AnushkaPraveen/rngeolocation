eaceb8048c05376541073dce1284e0066bab12fe
anushka <anushka@intervest.lk>
2022-07-06 11:31:28 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.mainContainer}>
+      <Text>Geoloacation</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer:{
+    flex:1,
+    justifyContent:'center',
+    alignSelf:'center'
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
