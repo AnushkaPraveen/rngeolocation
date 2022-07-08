a7a6e6c7006db60c9769b96c9b79af728f30d33d
anushka <anushka@intervest.lk>
2022-07-08 13:22:03 +0530

    background Location

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import {
   View,
   Button
 } from 'react-native';
-import Geolocation from 'react-native-geolocation-service';
+import BackgroundGeolocation from '@mauron85/react-native-background-geolocation';
 import {PermissionsAndroid} from 'react-native';
 
 const App = () => {
@@ -14,55 +14,133 @@ const App = () => {
   const [hasLocationPermission, setHasLocationPermission] = useState(false);
   const[locationData,setLocationData]=useState([])
 useEffect(()=>{
-  requestLocationPermission()
+  //requestLocationPermission()
  
 },[])
 
 
-  const requestLocationPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        /* {
-          title: 'Example App',
-          message: 'Example App access to your location ',
-        }, */
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('You can use the location');
-        alert('You can use the location');
-        setHasLocationPermission(true);
-        
-      } else {
-        console.log('location permission denied');
-        alert('Location permission denied');
+  const requestLocationPermission = () => {
+    BackgroundGeolocation.configure({
+      desiredAccuracy: BackgroundGeolocation.HIGH_ACCURACY,
+      stationaryRadius: 50,
+      distanceFilter: 50,
+      notificationTitle: 'Background tracking',
+      notificationText: 'enabled',
+      debug: true,
+      startOnBoot: false,
+      stopOnTerminate: true,
+      locationProvider: BackgroundGeolocation.ACTIVITY_PROVIDER,
+      interval: 10000,
+      fastestInterval: 5000,
+      activitiesInterval: 10000,
+      stopOnStillActivity: false,
+      url: 'http://192.168.81.15:3000/location',
+      httpHeaders: {
+        'X-FOO': 'bar'
+      },
+      // customize post properties
+      postTemplate: {
+        lat: '@latitude',
+        lon: '@longitude',
+        foo: 'bar' // you can also add your own properties
       }
-    } catch (err) {
-      console.warn(err);
-    }
+    });
+
+    BackgroundGeolocation.on('location', (location) => {
+      // handle your locations here
+      // to perform long running operation on iOS
+      // you need to create background task
+      BackgroundGeolocation.startTask(taskKey => {
+        // execute long running task
+        // eg. ajax post location
+        // IMPORTANT: task has to be ended by endTask
+        BackgroundGeolocation.endTask(taskKey);
+      });
+    });
+
+    BackgroundGeolocation.on('stationary', (stationaryLocation) => {
+      // handle stationary locations here
+      Actions.sendLocation(stationaryLocation);
+    });
+
+    BackgroundGeolocation.on('error', (error) => {
+      console.log('[ERROR] BackgroundGeolocation error:', error);
+    });
+
+    BackgroundGeolocation.on('start', () => {
+      console.log('[INFO] BackgroundGeolocation service has been started');
+    });
+
+    BackgroundGeolocation.on('stop', () => {
+      console.log('[INFO] BackgroundGeolocation service has been stopped');
+    });
+
+    BackgroundGeolocation.on('authorization', (status) => {
+      console.log('[INFO] BackgroundGeolocation authorization status: ' + status);
+      if (status !== BackgroundGeolocation.AUTHORIZED) {
+        // we need to set delay or otherwise alert may not be shown
+        setTimeout(() =>
+          Alert.alert('App requires location tracking permission', 'Would you like to open app settings?', [
+            { text: 'Yes', onPress: () => BackgroundGeolocation.showAppSettings() },
+            { text: 'No', onPress: () => console.log('No Pressed'), style: 'cancel' }
+          ]), 1000);
+      }
+    });
+
+    BackgroundGeolocation.on('background', () => {
+      console.log('[INFO] App is in background');
+    });
+
+    BackgroundGeolocation.on('foreground', () => {
+      console.log('[INFO] App is in foreground');
+    });
+
+    BackgroundGeolocation.on('abort_requested', () => {
+      console.log('[INFO] Server responded with 285 Updates Not Required');
+
+      // Here we can decide whether we want stop the updates or not.
+      // If you've configured the server to return 285, then it means the server does not require further update.
+      // So the normal thing to do here would be to `BackgroundGeolocation.stop()`.
+      // But you might be counting on it to receive location updates in the UI, so you could just reconfigure and set `url` to null.
+    });
+
+    BackgroundGeolocation.on('http_authorization', () => {
+      console.log('[INFO] App needs to authorize the http requests');
+    });
+
+    BackgroundGeolocation.checkStatus(status => {
+      console.log('[INFO] BackgroundGeolocation service is running', status.isRunning);
+      console.log('[INFO] BackgroundGeolocation services enabled', status.locationServicesEnabled);
+      console.log('[INFO] BackgroundGeolocation auth status: ' + status.authorization);
+
+      // you don't need to check status before start (this is just the example)
+      if (!status.isRunning) {
+        BackgroundGeolocation.start(); //triggers start on start event
+      }
+    });
   };  
 
-const getLocation=()=>{
+// const getLocation=()=>{
 
-  if (hasLocationPermission) {
-    Geolocation.getCurrentPosition(
-        (position) => {
-          setLocationData(position);
-          console.log(position);
-        },
-        (error) => {
-          // See error code charts below.
-          console.log(error.code, error.message);
-        },
-        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    );
-  }
-}
+//   if (hasLocationPermission) {
+//     Geolocation.getCurrentPosition(
+//         (position) => {
+//           setLocationData(position);
+//           console.log(position);
+//         },
+//         (error) => {
+//           // See error code charts below.
+//           console.log(error.code, error.message);
+//         },
+//         { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+//     );
+//   }
+// }
 
   return (
     <View style={styles.mainContainer}>
       <Text>react native geoloacation service</Text>
-       <Button title='Get Location' onPress={getLocation}/>  
+       <Button title='Get Location' onPress={requestLocationPermission}/>  
       {/* <Text>latitude : {locationData.coords.latitude }</Text> */}
     </View>
   );
